1b9d645424835d8566bcfcbb84e43d2266452541
chenshupeng888 <chenshupeng888@gmail.com>
2020-09-25 21:01:41 +0000

    code

---
+++ public/supported-code/code_for_cseR.R
@@ -0,0 +1,53 @@
+## packages we using 
+library(tidyverse)
+library(usethis)
+library(devtools)
+library(cesR)
+# data is getting from ces2019_web
+cesR::get_ces(srvy = "ces2019_web")
+
+## plots of education level 
+ces2019_web%>%
+  ggplot(aes(x=cps19_education))+geom_bar(colour="blue")+scale_x_binned(name="Education Level")+
+  labs(y="Number of people",title="Grap1: Education level among respondents",
+       caption= "Source:Stephenson, Laura B; Harell, Allison; Rubenson, Daniel; 
+       Loewen, Peter John, 2020, '2019 Canadian Election Study")+theme_bw()
+
+
+#### plot of satisfaction with government
+ces2019_web%>%
+  ggplot(aes(x=cps19_fed_gov_sat))+geom_histogram(stat="count")+scale_x_binned(name= "Satisfaction to Federal Government")+
+  labs(y="Number of people",title="Graph2: Respondents'Satisfaction with Federal Government",
+       caption= "Source:Stephenson, Laura B; Harell, Allison; Rubenson, Daniel; Loewen, Peter John, 2020,
+       '2019 Canadian Election Study")+theme_linedraw()
+
+#### group people whose educatiion level below high scool together with their satisfaction with government.
+ces2019_web_1<-
+  ces2019_web%>%
+  filter(cps19_education<4)
+ces2019_web_1<-
+  ces2019_web_1%>%
+  select(cps19_education,cps19_fed_gov_sat)
+ces2019_web_1%>%
+  ggplot(aes(x=cps19_education,y=cps19_fed_gov_sat))+geom_count()+
+  labs(y="Satisfaction with Federal Government",x="Education Level",
+       title="Graph3:Relationship between Satisfaction with Government and high Education",
+       caption= "Source:Stephenson, Laura; Harell, Allison; Rubenson, Daniel; Loewen, Peter, 2020, '2019 CES")+
+  theme_linedraw()
+
+
+### group people whose educatiion level above high scool together with their satisfaction with government.
+ces2019_web_2<-
+  ces2019_web%>%
+  filter(cps19_education>=4)
+ces2019_web_2<-
+  ces2019_web_2%>%
+  select(cps19_education,cps19_fed_gov_sat)
+ces2019_web_2%>%
+  ggplot(aes(x=cps19_education,y=cps19_fed_gov_sat))+
+  geom_count(colour="blue")+labs(y="Satisfaction with Federal Government",x="Education Level",
+                                 title="Graph4:Relationship between Satisfaction with Government and low Education",
+                                 caption= "Source: Stephenson, Laura; Harell,
+                                 Allison; Rubenson, Daniel; Loewen, Peter, 2020, '2019 CES")+theme_classic()
+
+#####################
